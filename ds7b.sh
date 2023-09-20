0343da4d38afee4e747e8a3652af23441ae9a3c9
revolutionarycomrade <78198382+revolutionarycomrade@users.noreply.github.com>
2023-09-20 18:17:00 +0700

    Update ds7b.sh

--- ds7b.sh
+++ ds7b.sh
@@ -1,6 +1,7 @@
 deepspeed qlora-ds.py \
     --model_name_or_path /mnt/c/models/daryl149_llama-2-7b-chat-hf \
     --output_dir ./output/llama-2-guanaco-7b \
+    --deepspeed /home/cjk/zero2.json
     --logging_steps 1 \
     --save_strategy steps \
     --data_seed 42 \
