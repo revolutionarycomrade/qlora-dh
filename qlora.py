205ff43a39277435d01a9886f62e31a8a2189206
revolutionarycomrade <78198382+revolutionarycomrade@users.noreply.github.com>
2023-12-20 18:48:05 +0700

    Update qlora.py

--- qlora.py
+++ qlora.py
@@ -213,7 +213,7 @@ def get_accelerate_model(args, checkpoint_dir):
         
     #max_memory = f'{args.max_memory_MB}MB'
     #max_memory = {i: max_memory for i in range(n_gpus)}
-    max_memory = {0: '24000MB', 1: '24000MB', 2: '24000MB'}
+    max_memory = {0: '24000MB', 1: '24000MB', 2: '20000MB'}
     device_map = "sequential" 
     '''device_map = {
         0: list(range(0, 27)),  # GPU 0 handles layers 0-26
